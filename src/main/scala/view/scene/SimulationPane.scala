d872cc4bde87932d5126bd8b2efce43c8e444f96
Martina <martina.cavallucci@studio.unibo.it>
2020-09-09 17:59:06 +0200

    Import messages actor

--- src/main/scala/view/scene/SimulationPane.scala
+++ src/main/scala/view/scene/SimulationPane.scala
@@ -2,13 +2,15 @@ package view.scene
 
 
 import akka.actor.{ActorSystem, Props}
+import model.Environment
 import scalafx.animation.{KeyFrame, Timeline}
 import scalafx.event.ActionEvent
-import scalafx.scene.control.{Button, Label, Separator, ToolBar}
+import scalafx.scene.control.{Button, Label, Separator, ToggleButton, ToolBar}
 import scalafx.scene.layout.{BorderPane, Pane}
 import scalafx.scene.paint.Color
 import scalafx.scene.text.Text
-import scalafx.util.Duration
+import scalafx.Includes._
+import utility.Messages.StartSimulation
 import view.actor.UiActor
 
 /**
@@ -19,24 +21,28 @@ case class SimulationPane() extends BorderPane {
   /* Custom canvas */
   private val canvas = new MyrmidonsCanvas
   private val system = ActorSystem("Myrmidons-system")
-  // private val uiActor = system.actorOf(Props(new UiActor(canvas, this)))
-  // private  val environment = system.actorOf(Props[Environment], name = "env-actor")
+  private val uiActor = system.actorOf(Props(new UiActor(canvas, this)))
+ // private val environment = system.actorOf(Props[Environment], name = "env-actor")
 
   /* ToolBar for manage ant simulation */
   private val toolBox: ToolBar = new ToolBar {
-    private val generation = new Text("0")
-    private val population = new Text("0")
-    private val startButton = new Button(text = "Start") {
-      // onAction = (_: ActionEvent) => uiActor ! StartSimulation(canvas.getAntCount)
+    val step = new Text("0")
+    val population = new Text("0")
+    private val startButton = new Button("Start") {
+      handleEvent(ActionEvent.Action) {
+        _: ActionEvent =>
+          uiActor ! StartSimulation(canvas.getAntCount)
+      }
     }
+
     val stopButton = new Button("Stop")
 
     content = List(
       startButton,
       stopButton,
       new Separator,
-      new Label("generation:"),
-      generation,
+      new Label("Step:"),
+      step,
       new Label("population:"),
       population)
   }
