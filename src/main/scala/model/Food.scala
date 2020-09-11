9d122de4de0822356cce280d27aa2165cf4f7ece
Simone Golinucci <simone.golinucci@studio.unibo.it>
2020-09-11 16:25:56 +0200

    * Add Anthill actor and his information * Add Anthill test * fix Obstacle class after Vector trait elimination * fix test that use Vector trait

--- src/main/scala/model/Food.scala
+++ src/main/scala/model/Food.scala
@@ -2,6 +2,6 @@ package model
 
 import utility.Geometry.Vector2D
 
-case class Food(position: Vector2D, quantity: Int)
+case class Food(override val position: Vector2D, quantity: Int)
   extends SimpleObstacle(position, quantity, quantity) {
 }
