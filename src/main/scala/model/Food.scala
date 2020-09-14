7f805abf342682b2eb0966dd5a2a3643df4e4623
Simone Golinucci <simone.golinucci@studio.unibo.it>
2020-09-14 14:26:43 +0200

    add food change status operations

--- src/main/scala/model/Food.scala
+++ src/main/scala/model/Food.scala
@@ -4,4 +4,12 @@ import utility.Geometry.Vector2D
 
 case class Food(override val position: Vector2D, quantity: Int)
   extends SimpleObstacle(position, quantity, quantity) {
+
+  def + (newQuantity: Int): Food = {
+    Food(position, quantity + newQuantity)
+  }
+
+  def - (newQuantity: Int): Food = {
+    this + ( - newQuantity)
+  }
 }
