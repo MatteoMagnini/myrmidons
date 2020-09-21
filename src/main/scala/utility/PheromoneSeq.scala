8ca76ada324db015607955a988b5fa89954dca16
Matteo Magnini - matteo.magnini@studio.unibo.it <matteo.magnini@studio.unibo.it>
2020-09-21 14:47:24 +0200

    Pheromone seq ok, now missing tests.

--- src/main/scala/utility/PheromoneSeq.scala
+++ src/main/scala/utility/PheromoneSeq.scala
@@ -9,11 +9,23 @@ object PheromoneSeq {
     def tick(): Seq[A] =
       seq.toStream.map(p => p.decrease).filter(opt => opt.isDefined).map(opt => opt.get.asInstanceOf[A])
 
-    def merge(threshold: Double): Seq[A] = ???
+    def add(newElement: A, threshold: Double = 1E-10): Seq[A] =
+      merge(newElement,threshold)
 
-    private def recursiveMerge(): Seq[A] = ???
+    private def merge(newElement: A, threshold: Double): Seq[A] =
+      recursiveMerge(newElement, threshold, seq) match {
+        case Some(x) => x +: seq
+        case None => newElement +: seq
+      }
 
-
+    @scala.annotation.tailrec
+    private def recursiveMerge(newElement: A, threshold: Double, sequence: Seq[A]): Option[A] =
+      if (sequence.nonEmpty) {
+        sequence.last.merge(newElement,threshold) match {
+          case Some(x) => Some(x.asInstanceOf[A])
+          case None => recursiveMerge(newElement, threshold, sequence.take(sequence.length - 1))
+        }
+      } else None
 
   }
 }
