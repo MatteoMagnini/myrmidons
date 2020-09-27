3f3d7d6af3670d486285537c95d000e20d521205
Linda Guiducci <linda.guiducci@studio.unibo.it>
2020-09-27 14:25:53 +0200

    Fix environment tests

--- src/main/scala/model/environment/Boundary.scala
+++ src/main/scala/model/environment/Boundary.scala
@@ -4,17 +4,27 @@ import utility.Geometry.Vector2D
 
 /** Environment boundary */
 trait Boundary {
+  import utility.Geometry.TupleOp._
 
-  def topLeft: Vector2D
-  def topRight: Vector2D
-  def bottomLeft: Vector2D
-  def bottomRight: Vector2D
+  def top: Double
+  def left: Double
+  def width: Double
+  def height: Double
+
+  /** Four corners of boundary */
+  def topLeft: Vector2D = (left, top)
+  def topRight: Vector2D = (left + width, top)
+  def bottomLeft: Vector2D = (left, top + height)
+  def bottomRight: Vector2D = (left + width, top + height)
 
   /** Returns the center of boundary */
-  def center:Vector2D
+  def center: Vector2D = (left + width / 2, top + height / 2)
 
   /** Returns whether a vector is inside boundaries */
-  def hasInside(pos: Vector2D): Boolean
+  def hasInside(pos: Vector2D): Boolean = {
+    (pos.x >= topLeft.x) && (pos.x <= topRight.x) &&
+      (pos.y >= topLeft.y) && (pos.y <= bottomLeft.y)
+  }
 }
 
 object Boundary {
@@ -29,19 +39,6 @@ object Boundary {
     * @param width environment width
     * @param height environment height
     */
-  private[this] class BoundaryImpl(left: Double, top: Double, width: Int, height: Int) extends Boundary {
-    import utility.Geometry.TupleOp._
-
-    override def topLeft: Vector2D = (left, top)
-    override def topRight: Vector2D = (left + width, top)
-    override def bottomLeft: Vector2D = (left, top + height)
-    override def bottomRight: Vector2D = (left + width, top + height)
-
-    override def center: Vector2D = (left + width / 2, top + height / 2)
-
-    override def hasInside(pos: Vector2D): Boolean = {
-      (pos.x >= left) && (pos.x <= topRight.x) &&
-        (pos.y >= top) && (pos.y <= bottomLeft.y)
-    }
-  }
+  private[this] class BoundaryImpl(override val left: Double, override val top: Double,
+                                   override val width: Double, override val height: Double) extends Boundary
 }
