37e5116f1d2b6117e33059b43e3c36f51ec10823
Matteo Magnini - matteo.magnini@studio.unibo.it <matteo.magnini@studio.unibo.it>
2020-10-12 15:14:05 +0200

    same reason

--- src/test/scala/utility/PrologTest.scala
+++ src/test/scala/utility/PrologTest.scala
@@ -10,25 +10,25 @@ class PrologTest extends AnyWordSpecLike with BeforeAndAfter {
 
   "Prolog engine" when {
 
+    val emptyTree = engine.toTerm("nil")
+    val node1 = engine.toTerm("node(1,range(0,4),range(0,4))")
+    val query1 = new Struct("insert",node1,emptyTree,new Var("R"))
+    val solution1 = engine.toTerm("tree(nil,node(1,range(0,4),range(0,4)),nil)")
+
     "adding a node to an empty tree" should {
-
-      val emptyTree = engine.toTerm("nil")
-      val node = engine.toTerm("node(1,range(0,4),range(0,4))")
-      val query = new Struct("insert",node,emptyTree,new Var("R"))
-      val result = engine.solve(query)
-      val solution = engine.toTerm("tree(nil,node(1,range(0,4),range(0,4)),nil)")
-
       "add it in the root" in {
-
-        assert(solution == result.getTerm("R"))
+        val result = engine.solve(query1)
+        assert(solution1 == result.getTerm("R"))
       }
     }
 
+    val node2 = engine.toTerm("node(2,range(2,6),range(8,12))")
+    val query2 = new Struct("insert",node2,solution1,new Var("R"))
     "adding a second node" should {
-
       "add a level in the tree" in {
-
-
+        val result = engine.solve(query2)
+        println(result.getTerm("R"))
+        //assert(solution1 == result.getTerm("R"))
       }
     }
 
